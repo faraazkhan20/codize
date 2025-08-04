d41ae1ef6eed28f3f0dc44af27ba1ac561564123
Faraaz Khan <faraazkhan.me@gmail.com>
2025-08-04 11:43:53 +0530

    Updated src/pages/Arrays.jsx

--- src/pages/Arrays.jsx
+++ src/pages/Arrays.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 import Navbar from "../components/Navbar";
 
@@ -15,8 +17,88 @@ function Arrays() {
           <li className="content">Array के elements contiguous memory locations में इसलिए store होते हैं ताकि data access तेज़ और efficient हो सके। जब किसी array को declare किया जाता है, तो compiler उसे एक continuous memory block allocate करता है। इससे हर element का address आसानी से calculate किया जा सकता है, जैसे: base_address + index * size_of_datatype। यह predictable addressing CPU cache के साथ बेहतर काम करता है और traversal को fast बनाता है। साथ ही, sequential access (जैसे loops) में performance बेहतर मिलती है क्योंकि processor prefetching आसानी से कर पाता है। यही वजह है कि arrays को contiguous memory में store किया जाता है।</li>
         </ul>
 
+        <button type="button" className="btn btn-sm btn-outline-primary mb-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          Show
+        </button>
+
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content modal-dark">
+              <div className="modal-header p-1 justify-content-center" style={{ borderBottom: "0px" }}>
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Code
+                </h5>
+                {/* <button type="button" className="btn-close btn-white" data-bs-dismiss="modal" aria-label="Close"></button> */}
+              </div>
+              <div className="modal-body px-1 py-0">
+                <SyntaxHighlighter language="cpp" style={vscDarkPlus} className="rounded">
+                  {`int main() {
+  int arr[] = {1, 2, 3, 4, 5};
+  int max = INT_MIN;
+  for(int i = 0; i < 5; i++) {
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  cout << "Max: " << max;
+  return 0;
+}`}
+                </SyntaxHighlighter>
+              </div>
+              <div class="modal-footer p-1" style={{ borderTop: "0px" }}>
+                <button type="button" class="btn btn-sm btn-outline-danger" data-bs-dismiss="modal">
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <h6>why arr(s) are required?</h6>
         <p className="content">Array से हम multiple values को एक ही नाम से store कर सकते हैं, जिससे memory management और data access आसान होता है।</p>
+
+        {/* Table */}
+        <div className="table-responsive">
+          <table className="table table-dark table-bordered">
+            <tbody>
+              <tr>
+                <td>1,1</td>
+                <td>1,2</td>
+                <td>1,3</td>
+                <td>1,4</td>
+                <td>1,5</td>
+              </tr>
+              <tr>
+                <td>2,1</td>
+                <td>2,2</td>
+                <td>2,3</td>
+                <td>2,4</td>
+                <td>2,5</td>
+              </tr>
+              <tr>
+                <td>3,1</td>
+                <td>3,2</td>
+                <td>3,3</td>
+                <td>3,4</td>
+                <td>3,5</td>
+              </tr>
+              <tr>
+                <td>4,1</td>
+                <td>4,2</td>
+                <td>4,3</td>
+                <td>4,4</td>
+                <td>4,5</td>
+              </tr>
+              <tr>
+                <td>5,1</td>
+                <td>5,2</td>
+                <td>5,3</td>
+                <td>5,4</td>
+                <td>5,5</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,7 @@ function NotFound() {
     <>
       <Navbar />
       <div className="container mt-4">
-        <h4 className="text-center mb-0">Error 404</h4>
+        <h4 className="text-center mb-0">Error 404;</h4>
         <h6 className="text-center">Page Not Found</h6>
       </div>
     </>
