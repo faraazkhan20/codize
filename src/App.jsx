7eb0c3f1935021bbc26757728f10590f8b9cc0d0
Faraaz Khan <faraazkhan.me@gmail.com>
2025-08-04 09:09:02 +0530

    Updated global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import cardsData from "./data/cardsData";
 
 function App() {
   return (
@@ -6,12 +7,13 @@ function App() {
       <Navbar />
       <div className="container mt-4">
         <div className="row">
-          {[...Array(6)].map((_, i) => (
-            <div className="col-lg-4 col-md-6 col-sm-6 col-6 mb-4" key={i}>
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Card {i + 1}</h5>
-                  <p className="card-text">Card content.</p>
+          {cardsData.map((card, i) => (
+            <div className="col-lg-2 col-md-3 col-sm-4 col-6 mb-4" key={i}>
+              <div className="card h-100 bg-transparent border-2 rounded shadow" style={{ borderColor: card.color }}>
+                <div className="card-body h-100 text-center d-flex justify-content-center align-items-center p-3">
+                  <div className="card-title m-0" style={{ color: card.color }}>
+                    {card.title}
+                  </div>
                 </div>
               </div>
             </div>
---
+++ src/data/cardsData.js
@@ -0,0 +1,10 @@
+const cardsData = [
+  { title: "Arrays", color: "red" },
+  { title: "Time and Space Complexity", color: "yellow" },
+  { title: "Sorting", color: "cyan" },
+  { title: "Binary Search", color: "green" },
+  { title: "Vectors", color: "pink" },
+  { title: "Strings", color: "tomato" },
+];
+
+export default cardsData;
